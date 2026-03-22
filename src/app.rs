08585371bed558588d66ef44d26617358c6d81b4
555-Roman <di.elle.vi11@gmail.com>
2026-03-22 21:28:44 +0100

    App/window abstraction

---
+++ src/app.rs
@@ -0,0 +1,161 @@
+use std::num::NonZeroU32;
+use std::process::abort;
+use glow::Context;
+use glutin::config::{Config, ConfigTemplateBuilder};
+use glutin::context::{ContextAttributesBuilder, PossiblyCurrentContext};
+use glutin::display::{GetGlDisplay};
+use glutin::prelude::{GlConfig, GlDisplay, NotCurrentGlContext, PossiblyCurrentGlContext};
+use glutin::surface::{GlSurface, Surface, SwapInterval, WindowSurface};
+use glutin_winit::{DisplayBuilder, GlWindow};
+use log::error;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::raw_window_handle::HasWindowHandle;
+use winit::window::{Window, WindowAttributes, WindowId};
+
+pub struct App {
+    pub window: Option<Window>,
+    pub gl_surface: Option<Surface<WindowSurface>>,
+    pub gl_context: Option<PossiblyCurrentContext>,
+    pub gl: Option<Context>,
+
+    pub title: String,
+    pub render: *const unsafe fn(&Context),
+}
+
+pub fn new_app(title: &str, render: unsafe fn(&Context)) -> App {
+    App {
+        window: None,
+        gl_surface: None,
+        gl_context: None,
+        gl: None,
+
+        title: title.to_string(),
+        render: render as *const unsafe fn(&Context),
+    }
+}
+
+pub fn run_app(mut app: App) {
+    let _ = EventLoop::new().unwrap().run_app(&mut app);
+}
+
+fn gl_config_picker(configs: Box<dyn Iterator<Item = Config> + '_>) -> Config {
+    configs
+        .reduce(|accum, config| {
+            let transparency_check = config.supports_transparency().unwrap_or(false)
+                & !accum.supports_transparency().unwrap_or(false);
+
+            if transparency_check || config.num_samples() > accum.num_samples() {
+                config
+            } else {
+                accum
+            }
+        })
+        .unwrap()
+}
+
+fn window_attributes(title: String) -> WindowAttributes {
+    Window::default_attributes()
+        .with_transparent(true)
+        .with_title(title)
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let template = ConfigTemplateBuilder::new().with_alpha_size(8).with_transparency(false);
+
+        let display_builder = DisplayBuilder::new().with_window_attributes(Some(window_attributes(self.title.clone())));
+        let (window, gl_config) = match display_builder.build(
+            event_loop,
+            template,
+            gl_config_picker,
+        ) {
+            Ok((window, gl_config)) => (window.unwrap(), gl_config),
+            Err(_err) => {
+                event_loop.exit();
+                return;
+            },
+        };
+
+        let raw_window_handle = window.window_handle().unwrap_or_else(|err| {
+            error!("why tf did that not work?? {:?}", err);
+            abort()
+        }).as_raw();
+
+        let context_attributes = ContextAttributesBuilder::new().build(Some(raw_window_handle));
+        let gl_display = gl_config.display();
+
+        unsafe {
+            let gl_context = gl_display.create_context(&gl_config, &context_attributes).unwrap_or_else(|_| {
+                error!("ur device is too old noob, skill issue");
+                abort()
+            }).treat_as_possibly_current();
+
+            let attrs = window
+                .build_surface_attributes(Default::default())
+                .unwrap_or_else(|_| {
+                    error!("ur hardware dont work or smth prob too old skill issue");
+                    abort()
+                });
+            let gl_surface = gl_config.display().create_window_surface(&gl_config, &attrs).unwrap();
+
+            gl_context.make_current(&gl_surface).unwrap();
+
+            let gl = Context::from_loader_function_cstr(|s| gl_display.get_proc_address(s));
+
+            gl_surface
+                .set_swap_interval(&gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap()))
+                .unwrap();
+
+            self.gl_surface = Some(gl_surface);
+            self.gl_context = Some(gl_context);
+            self.gl = Some(gl);
+        }
+
+        self.window = Some(window);
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            },
+            WindowEvent::KeyboardInput {event, .. } => {
+                println!("Received keyboard input");
+
+                if event.state == winit::event::ElementState::Pressed {
+                    match event.logical_key.to_text() {
+                        None => {}
+                        Some(char) => println!("  Received key: {:?}", char)
+                    }
+
+                }
+            },
+            WindowEvent::Resized(size) if size.width != 0 && size.height != 0 => {
+                let width = unsafe { NonZeroU32::new_unchecked(size.width) };
+                let height = unsafe { NonZeroU32::new_unchecked(size.height) };
+
+                let context = self.gl_context.as_ref().unwrap();
+                let surface = self.gl_surface.as_ref().unwrap();
+
+                surface.resize(context, width, height);
+            },
+            WindowEvent::RedrawRequested => {
+                let window = self.window.as_ref().unwrap();
+                let context = self.gl_context.as_ref().unwrap();
+                let surface = self.gl_surface.as_ref().unwrap();
+                let gl = self.gl.as_ref().unwrap();
+
+                unsafe {
+                    let render = std::mem::transmute::<*const unsafe fn(&Context), unsafe fn(&Context)>(self.render);
+                    render(gl);
+                }
+
+                window.request_redraw();
+                surface.swap_buffers(context).unwrap();
+            },
+            _ => (),
+        }
+    }
+}
