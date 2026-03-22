ddf168270498c5826954c063ec9275a3ff2a1c47
555-Roman <di.elle.vi11@gmail.com>
2026-03-22 16:30:58 +0100

    Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use winit;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowAttributes, WindowId};
+
+struct App {
+    window: Window,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        println!("Resumed!");
+
+        self.window = event_loop.create_window(WindowAttributes::default()).unwrap();
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
+        println!("Event!");
+
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            },
+            WindowEvent::KeyboardInput {device_id, event, is_synthetic} => {
+                println!("Received keyboard input");
+
+                if event.state == winit::event::ElementState::Pressed {
+                    let char = event.logical_key.to_text().unwrap();
+                    println!("  Received key: {:?}", char);
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = event_loop.create_window(WindowAttributes::default()).unwrap();
+    let mut app = App{window};
+
+    event_loop.run_app(&mut app).unwrap();
 }
